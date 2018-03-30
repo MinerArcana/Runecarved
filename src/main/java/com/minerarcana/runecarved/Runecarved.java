ee1131d20ca2efe0b08ef58baa446132ec486530
warlordjones <warlordjones@gmail.com>
2018-03-30 21:00:19 +0100

    Project setup

---
+++ src/main/java/com/minerarcana/runecarved/Runecarved.java
@@ -0,0 +1,23 @@
+package com.minerarcana.runecarved;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Runecarved.MODID, name = Runecarved.NAME, version = Runecarved.VERSION)
+public class Runecarved {
+	public static final String MODID = "runecarved";
+	public static final String NAME = "Runecarved";
+	public static final String VERSION = "0.0.0";
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+
+	}
+}
