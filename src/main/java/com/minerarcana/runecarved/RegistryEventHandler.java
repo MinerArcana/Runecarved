a9fe38db520b1c281c3853c159a089b532ec971c
warlordjones <warlordjones@gmail.com>
2018-04-01 13:06:47 +0100

    Fix automatic enchantment registration

--- src/main/java/com/minerarcana/runecarved/RegistryEventHandler.java
+++ src/main/java/com/minerarcana/runecarved/RegistryEventHandler.java
@@ -2,9 +2,12 @@ package com.minerarcana.runecarved;
 
 import com.minerarcana.runecarved.api.RunecarvedAPI;
 import com.minerarcana.runecarved.api.spell.SpellRegistryEvent;
+import com.minerarcana.runecarved.enchantments.EnchantmentSpell;
 import com.minerarcana.runecarved.item.ItemRuneStone;
 import com.minerarcana.runecarved.spell.*;
 import com.teamacronymcoders.base.registrysystem.ItemRegistry;
+
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -13,12 +16,19 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @EventBusSubscriber(modid = Runecarved.MODID)
 public class RegistryEventHandler {
+
+    @SubscribeEvent
+    public static void createSpellEnchantments(RegistryEvent.Register<Enchantment> event) {
+        RunecarvedAPI.getInstance().getSpellRegistry().getSpells().values()
+                .forEach(spell -> event.getRegistry().register(new EnchantmentSpell()
+                        .setRegistryName(spell.getRegistryName()).setName(spell.getRegistryName().getResourcePath())));
+    }
+
     @SubscribeEvent(priority = EventPriority.HIGH)
     public static void createSpellItems(RegistryEvent.Register<Item> itemRegisterEvent) {
         ItemRegistry itemRegistry = Runecarved.instance.getRegistry(ItemRegistry.class, "ITEM");
 
-        RunecarvedAPI.getInstance().getSpellRegistry().getSpells().values().stream()
-                .map(ItemRuneStone::new)
+        RunecarvedAPI.getInstance().getSpellRegistry().getSpells().values().stream().map(ItemRuneStone::new)
                 .forEach(itemRegistry::register);
     }
 
--- src/main/java/com/minerarcana/runecarved/enchantments/EnchantmentSpell.java
+++ src/main/java/com/minerarcana/runecarved/enchantments/EnchantmentSpell.java
@@ -1,13 +1,23 @@
 package com.minerarcana.runecarved.enchantments;
 
+import com.minerarcana.runecarved.Runecarved;
+
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnumEnchantmentType;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 
 public class EnchantmentSpell extends Enchantment {
 
+    @ObjectHolder(value = Runecarved.MODID + ":scroll")
+    public static final Item scroll = null;
+
+    public static final EnumEnchantmentType SPELL = EnumHelper.addEnchantmentType("spells", item -> item == scroll);
+
     public EnchantmentSpell() {
-        super(Enchantment.Rarity.RARE, EnchantmentHandler.SPELL,
-                new EntityEquipmentSlot[] { EntityEquipmentSlot.MAINHAND });
+        super(Enchantment.Rarity.RARE, SPELL, new EntityEquipmentSlot[] { EntityEquipmentSlot.MAINHAND });
     }
 
 }
