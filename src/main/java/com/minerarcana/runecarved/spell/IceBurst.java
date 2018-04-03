42e6e27b1fe4be3baa44d3830330c08589684d6e
warlordjones <warlordjones@gmail.com>
2018-04-03 17:24:33 +0100

    Implement lightning bolt spell
    Suffers from known bug of projectile impacting caster

--- src/main/java/com/minerarcana/runecarved/spell/IceBurst.java
+++ src/main/java/com/minerarcana/runecarved/spell/IceBurst.java
@@ -1,22 +1,29 @@
 package com.minerarcana.runecarved.spell;
 
-import com.minerarcana.runecarved.api.entity.EntityProjectileSpell;
-import com.minerarcana.runecarved.api.spell.ProjectileSpell;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.monster.EntityBlaze;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import static com.minerarcana.runecarved.Runecarved.MODID;
 
 import javax.annotation.Nonnull;
 
-import static com.minerarcana.runecarved.Runecarved.MODID;
+import com.minerarcana.runecarved.api.caster.ICaster;
+import com.minerarcana.runecarved.api.entity.EntityProjectileSpell;
+import com.minerarcana.runecarved.api.spell.ProjectileSpell;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.EntityBlaze;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class IceBurst extends ProjectileSpell {
     public IceBurst() {
         super(new ResourceLocation(MODID, "ice_burst"));
+    }
+
+    @Override
+    public void cast(ICaster caster) {
+        for (int i = 0; i < 3; i++) {
+            super.cast(caster);
+        }
     }
 
     @Override
@@ -27,7 +34,7 @@ public class IceBurst extends ProjectileSpell {
     @Override
     public void onImpact(EntityProjectileSpell entitySpell, @Nonnull Entity entity) {
         entity.attackEntityFrom(DamageSource.causeThrownDamage(entitySpell, entitySpell.getThrower()),
-                //TODO Spell Resistances
+                // TODO Spell Resistances
                 entity instanceof EntityBlaze ? 6f : 3f);
     }
 
--- src/main/java/com/minerarcana/runecarved/spell/LightningBolt.java
+++ src/main/java/com/minerarcana/runecarved/spell/LightningBolt.java
@@ -1,18 +1,44 @@
 package com.minerarcana.runecarved.spell;
 
-import com.minerarcana.runecarved.api.caster.ICaster;
-import com.minerarcana.runecarved.api.spell.Spell;
-import net.minecraft.util.ResourceLocation;
-
 import static com.minerarcana.runecarved.Runecarved.MODID;
 
-public class LightningBolt extends Spell {
+import com.minerarcana.runecarved.api.entity.EntityProjectileSpell;
+import com.minerarcana.runecarved.api.spell.ProjectileSpell;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class LightningBolt extends ProjectileSpell {
     public LightningBolt() {
         super(new ResourceLocation(MODID, "lightning_bolt"));
     }
 
     @Override
-    public void cast(ICaster caster) {
+    public EntityProjectileSpell getEntityProjectileSpell(World world) {
+        return new EntityProjectileSpell(world, this);
+    }
+
+    @Override
+    public void onImpact(EntityProjectileSpell entitySpell, Entity entity) {
+        if (entity instanceof EntityLivingBase) {
+            EntityLivingBase living = ((EntityLivingBase) entity);
+            living.addPotionEffect(
+                    new PotionEffect(Potion.getPotionFromResourceLocation("slowness"), 20, 3, true, false));
+            living.addPotionEffect(
+                    new PotionEffect(Potion.getPotionFromResourceLocation("blindness"), 20, 1, true, false));
+        }
+    }
+
+    @Override
+    public void onImpact(EntityProjectileSpell entitySpell, BlockPos pos, EnumFacing impactedSize) {
+        // TODO Auto-generated method stub
 
     }
+
 }
